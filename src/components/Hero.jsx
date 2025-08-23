4d3510ef12d351815dc2bb2fe50ad337e6410118
Lauromon <orbesle@gmail.com>
2025-08-23 19:53:09 -0300

    add basic responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,21 +26,21 @@ function Hero() {
     return (
         <header id='home' className='flex justify-center items-center max-w-screen h-screen  bg-cover w-full bg-no-repeat p-16' style={{ backgroundImage: `url(${heroBg})` }}>
             <section className='max-w-5xl h-[80%] flex flex-col justify-center bg-gray/10 px-16 rounded-3xl gap-4'>
-
-                <div className='flex gap-4 mb-4'>
-                    <div className="text-left text-lg-left justify-center flex flex-col gap-4">
-                        <h1 className='text-3xl font-bold'>Hello there, I'm</h1>
-                        <span className='font-extrabold text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-r from-[#0BCEAF] to-purple-200 -mt-5'>Lautaro Orbes</span>
-                        <div className='flex'>
-                            <h2 className='typed-element font-bold text-4xl d-none text-transparent' style={{ WebkitTextStroke: '1px #fff' }} ref={typedElement} >
+<div className='flex flex-col lg:flex-row items-center lg:items-start gap-4 mb-4'>
+    <div className="text-left text-lg-left justify-center flex flex-col gap-4 order-last lg:order-none">
+        <h1 className='text-2xl md:text-3xl font-bold'>Hello there, I'm</h1>
+        <span className='font-extrabold text-transparent text-4xl md:text-5xl lg:text-7xl bg-clip-text bg-gradient-to-r from-[#0BCEAF] to-purple-200 -mt-5'>Lautaro Orbes</span>
+        <div className='flex'>
+            <h2 className='typed-element font-bold text-3xl md:text-4xl d-none text-transparent' style={{ WebkitTextStroke: '1px #fff' }} ref={typedElement}>
+        
                             </h2>
                         </div>
                     </div>
-                    <div className='flex justify-center min-w-fit'>
-                        <img className='rounded-full shadow-lg size-80 hover:bg-cyan-300/10' src={perfil} alt="Foto de perfil" />
+                    <div className='flex justify-center min-w-fit order-first lg:order-none'>
+                        <img className='rounded-full shadow-lg size-48 md:size-64 lg:size-80 hover:bg-cyan-300/10' src={perfil} alt="Foto de perfil" />
                     </div>
                 </div>
-                <div className='flex gap-4 justify-start'>
+                <div className='flex gap-4 justify-center lg:justify-start'>
 
                     <SocialPill href="https://github.com/LauroMon">
                         <Github height={30} width={30} />
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -7,7 +7,7 @@ import ContactForm from "../components/ContactForm";
 
 export default function Index() {
     return (
-        <div className='flex flex-col items-center max-w-screen bg-gradient-to-b from-black via-gray-800 to-cyan-600 text-white pb-8'>
+        <div className='flex flex-col items-center max-w-screen px-4 mx-auto bg-gradient-to-b from-black via-gray-800 to-cyan-600 text-white pb-8'>
             <Navbar />
             <Hero />
             <Experience />
